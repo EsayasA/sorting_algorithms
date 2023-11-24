e30343fecbe4ce30b8ba6f336378b82e6d9a02df
EsayasA <esayasaregawi29@gmail.com>
2023-11-24 06:34:03 -0800

    no 1

--- sort.h
+++ sort.h
@@ -1,12 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
-
 #include <stdio.h>
 #include <stdlib.h>
-
 #define UP 0
 #define DOWN 1
-
 /**
  * enum bool - Enumeration of Boolean values.
  * @false: Equals 0.
@@ -17,13 +14,11 @@ typedef enum bool
 	false = 0,
 	true
 } bool;
-
 /**
  * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
+ * @n:integer
+ * @prev:to the previous element
+ * @next:to the next element
  */
 typedef struct listint_s
 {
@@ -32,11 +27,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -49,5 +42,4 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
-#endif /* SORT_H */
+#endif
